bd12c69327750d56fef1072929c2a789c3b40662
Patrick <44092756+vigetious@users.noreply.github.com>
2021-05-08 10:15:14 +0100

    CSSing streamer list page

--- ClientApp/src/components/Streamers/Streamers.js
+++ ClientApp/src/components/Streamers/Streamers.js
@@ -1,14 +1,64 @@
 import React, {useState, useEffect} from "react";
 import StreamersRows from "./StreamersRows";
+import {
+    AppBar, //Grid,
+    GridList,
+    GridListTile,
+    GridListTileBar,
+    isWidthUp,
+    makeStyles, Slider,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
 
+import Streamer from "./Streamer/Streamer";
+
+const styles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        overflow: "hidden",
+        width: "100%"
+    },
+    iconSizeSlider: {
+        width: 100
+    }
+}));
 
 export default function Streamers() {
-    const [streamers, setStreamers] = useState([])
-    
+    const [streamers, setStreamers] = useState([]);
+    const [iconSize, setIconSize] = useState(getIconSizeCookie);
+    const [defaultSliderValue] = useState(getDefaultSliderValue);
+    const classes = styles();
+
     useEffect(() => {
         getStreamers();
     }, [])
     
+    function getIconSizeCookie() {
+        var iconSizeCookie;
+        iconSizeCookie = parseInt(getCookieValue("iconSize"));
+        return iconSizeCookie != null ? iconSizeCookie : 300;
+    }
+    
+    function getDefaultSliderValue() {
+        var defaultSliderValueCookie;
+        defaultSliderValueCookie = parseInt(getCookieValue("defaultSliderValue"));
+        return defaultSliderValueCookie != null ? defaultSliderValueCookie : 2;
+    }
+    
+    function getCookieValue(key) {
+        if (document.cookie != null) {
+            var split = document.cookie.split(";")
+            if (split != null) {
+                var find = split.find(row => row.replaceAll(" ", "").startsWith(key + "="))
+                if (find != null) {
+                    return find.split("=")[1];
+                }
+            }
+        }
+    }
+
     async function getStreamers() {
         const request = await fetch('database/streamers',
             {
@@ -19,17 +69,58 @@ export default function Streamers() {
             });
 
         var response = await request.json();
-        
         setStreamers(response.data);
     }
     
+    function changeIconSize(event, value) {
+        switch (value) {
+            case 1:
+                setIconSize(250);
+                document.cookie = "iconSize=" + 250 + "; SameSite=None; Secure";
+                document.cookie = "defaultSliderValue=" + 1 + "; SameSite=None; Secure";
+                break;
+            case 2:
+                setIconSize(300);
+                document.cookie = "iconSize=" + 300 + "; SameSite=None; Secure";
+                document.cookie = "defaultSliderValue=" + 2 + "; SameSite=None; Secure";
+                break;
+            case 3:
+                setIconSize(350);
+                document.cookie = "iconSize=" + 350 + "; SameSite=None; Secure";
+                document.cookie = "defaultSliderValue=" + 3 + "; SameSite=None; Secure";
+                break;
+            case 4:
+                setIconSize(400);
+                document.cookie = "iconSize=" + 400 + "; SameSite=None; Secure";
+                document.cookie = "defaultSliderValue=" + 4 + "; SameSite=None; Secure";
+                break;
+            default:
+                setIconSize(300);
+                document.cookie = "iconSize=" + 300 + "; SameSite=None; Secure";
+                document.cookie = "defaultSliderValue=" + 2 + "; SameSite=None; Secure";
+        }
+    }
+
+
     return (
-        <div>
-            <table>
-                <tbody>
-                {streamers.map(streamer => <StreamersRows key={streamer.id} passedStreamer={streamer} />)}
-                </tbody>
-            </table>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <div className={classes.iconSizeSlider}>
+                        <Slider defaultValue={defaultSliderValue}
+                                aria-labelledby="discrete-slider"
+                                valueLabelDisplay="auto"
+                                step={1}
+                                min={1}
+                                max={4}
+                                onChange={changeIconSize}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <GridList cellHeight={300}>
+                {streamers.map(streamer => (<StreamersRows key={streamer.id} passedStreamer={streamer} iconSize={iconSize} />))}
+            </GridList>
         </div>
     )
 }
--- ClientApp/src/components/Streamers/StreamersRows.js
+++ ClientApp/src/components/Streamers/StreamersRows.js
@@ -1,15 +1,29 @@
 import React, {useState, useEffect} from "react";
+import {GridListTile, GridListTileBar, Link, makeStyles} from "@material-ui/core";
+
+const styles = makeStyles((theme) => ({
+    GridListTile: {
+        //width: 300,
+        //height: 300,
+        padding: 2
+    },
+    topTileBar: {
+        backgroundColor: "unset"
+    }
+}));
 
 export default function StreamersRows(passedStreamer) {
     const [streamer] = useState(passedStreamer.passedStreamer);
-    const [streamerUrl] = useState("/streamer/" + streamer.id)
+    const [streamerUrl] = useState("/streamer/" + streamer.id);
+    const classes = styles();
 
     return (
-        <tr>
-            <a href={streamerUrl}>
-            <td><img className={'thumbnail'} alt="thumbnail" src={streamer.thumbnailLocation}/></td>
-            <td>{streamer.displayName}</td>
+        <GridListTile cols={2} className={classes.GridListTile} key={passedStreamer.key}>
+            <a style={{ display: "block" }} href={streamerUrl}>
+                <GridListTileBar className={classes.topTileBar} titlePosition={"top"} title={streamer.isLive ? "🔴 LIVE": null} />
+                <img alt="thumbnail" style={{ width: passedStreamer.iconSize, height: passedStreamer.iconSize}} src={streamer.thumbnailLocation}/>
+                <GridListTileBar title={streamer.displayName} />
             </a>
-        </tr>
+        </GridListTile>
     )
 }
