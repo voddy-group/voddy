2f91873b0fe4fb03692681a7a06e415c63d8eaab
Patrick <44092756+vigetious@users.noreply.github.com>
2021-05-29 11:40:25 +0100

    seperating logic and controller

--- Controllers/Streams/DeleteStreams.cs
+++ Controllers/Streams/DeleteStreams.cs
@@ -13,79 +13,23 @@ namespace voddy.Controllers.Streams {
     [ApiController]
     [Route("streams")]
     public class DeleteStreams : ControllerBase {
-        private readonly IWebHostEnvironment _environment;
-        private readonly IHubContext<NotificationHub> _hubContext;
+        private readonly DeleteStreamsLogic _deleteStreamsLogic;
 
-        public DeleteStreams(IWebHostEnvironment environment, IHubContext<NotificationHub> hubContext) {
-            _environment = environment;
-            _hubContext = hubContext;
+        public DeleteStreams() {
+            _deleteStreamsLogic = new DeleteStreamsLogic();
         }
 
         [HttpDelete]
         [Route("deleteStream")]
         public DeleteStreamReturn DeleteSingleStream(long streamId) {
-            using (var context = new DataContext()) {
-                var stream = context.Streams.FirstOrDefault(item => item.streamId == streamId);
-                var chat = context.Chats.Where(item => item.streamId == streamId).ToList();
-
-                if (stream != null) {
-                    if (stream.downloadJobId != null) {
-                        BackgroundJob.Delete(stream.downloadJobId);
-                    }
-
-                    CleanUpStreamFiles(stream.streamId, stream.streamerId);
-                    context.Remove(stream);
-
-                    if (stream.chatDownloadJobId != null) {
-                        BackgroundJob.Delete(stream.chatDownloadJobId);
-                        for (var x = 0; x < chat.Count; x++) {
-                            context.Remove(chat[x]);
-                        }
-                    }
-                }
-
-                context.SaveChanges();
-            }
-
-            TwitchApiHelpers twitchApiHelpers = new TwitchApiHelpers();
-            var request =
-                twitchApiHelpers.TwitchRequest("https://api.twitch.tv/helix/videos?id=" + streamId, Method.GET);
-
-            HandleDownloadStreamsLogic handleDownloadStreamsLogic = new HandleDownloadStreamsLogic();
-            _hubContext.Clients.All.SendAsync("ReceiveMessage",
-                handleDownloadStreamsLogic.CheckForDownloadingStreams());
-
-            if (request.StatusCode == HttpStatusCode.OK) {
-                return new DeleteStreamReturn {
-                    isStillAvailable = true
-                };
-            }
-
-            return new DeleteStreamReturn {
-                isStillAvailable = false
-            };
-        }
-
-        public void CleanUpStreamFiles(long streamId, int streamerId) {
-            Directory.Delete($"{_environment.ContentRootPath}streamers/{streamerId}/vods/{streamId}", true);
+            return _deleteStreamsLogic.DeleteSingleStreamLogic(streamId);
         }
 
         [HttpDelete]
         [Route("deleteStreams")]
         public IActionResult DeleteStreamerStreams(int streamerId) {
-            using (var context = new DataContext()) {
-                var streamerStreams = context.Streams.Where(item => item.streamerId == streamerId).ToList();
-
-                for (var x = 0; x < streamerStreams.Count; x++) {
-                    DeleteSingleStream(streamerStreams[x].streamId);
-                }
-            }
-
+            _deleteStreamsLogic.DeleteStreamerStreamsLogic(streamerId);
             return Ok();
         }
-    }
-
-    public class DeleteStreamReturn {
-        public bool isStillAvailable { get; set; }
     }
 }
---
+++ Controllers/Streams/DeleteStreamsLogic.cs
@@ -0,0 +1,71 @@
+using System.IO;
+using System.Linq;
+using System.Net;
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using voddy.Data;
+
+namespace voddy.Controllers.Streams {
+    public class DeleteStreamsLogic {
+        public DeleteStreamReturn DeleteSingleStreamLogic(long streamId) {
+            using (var context = new DataContext()) {
+                var stream = context.Streams.FirstOrDefault(item => item.streamId == streamId);
+                var chat = context.Chats.Where(item => item.streamId == streamId).ToList();
+
+                if (stream != null) {
+                    if (stream.downloadJobId != null) {
+                        BackgroundJob.Delete(stream.downloadJobId);
+                    }
+
+                    string contentRootPath =
+                        context.Configs.FirstOrDefault(item => item.key == "contentRootPath").value;
+
+                    CleanUpStreamFiles(contentRootPath, stream.streamId, stream.streamerId);
+                    context.Remove(stream);
+
+                    if (stream.chatDownloadJobId != null) {
+                        BackgroundJob.Delete(stream.chatDownloadJobId);
+                        for (var x = 0; x < chat.Count; x++) {
+                            context.Remove(chat[x]);
+                        }
+                    }
+                }
+
+                context.SaveChanges();
+            }
+
+            TwitchApiHelpers twitchApiHelpers = new TwitchApiHelpers();
+            var request =
+                twitchApiHelpers.TwitchRequest("https://api.twitch.tv/helix/videos?id=" + streamId, Method.GET);
+
+            if (request.StatusCode == HttpStatusCode.OK) {
+                return new DeleteStreamReturn {
+                    isStillAvailable = true
+                };
+            }
+
+            return new DeleteStreamReturn {
+                isStillAvailable = false
+            };
+        }
+
+        public void DeleteStreamerStreamsLogic(int streamerId) {
+            using (var context = new DataContext()) {
+                var streamerStreams = context.Streams.Where(item => item.streamerId == streamerId).ToList();
+
+                for (var x = 0; x < streamerStreams.Count; x++) {
+                    DeleteSingleStreamLogic(streamerStreams[x].streamId);
+                }
+            }
+        }
+
+        public void CleanUpStreamFiles(string contentRootPath, long streamId, int streamerId) {
+            Directory.Delete($"{contentRootPath}streamers/{streamerId}/vods/{streamId}", true);
+        }
+    }
+
+    public class DeleteStreamReturn {
+        public bool isStillAvailable { get; set; }
+    }
+}
