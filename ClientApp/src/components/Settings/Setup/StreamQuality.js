25fa149ab708d802cb1598abc39eb13f650c40e3
Patrick <44092756+vigetious@users.noreply.github.com>
2021-08-10 21:23:02 +0100

    made changing global stream quality a bit more streamline

--- ClientApp/src/components/Settings/Setup/StreamQuality.js
+++ ClientApp/src/components/Settings/Setup/StreamQuality.js
@@ -1,53 +1,19 @@
 import React, {useState, useEffect} from "react";
-import loading from "../../../assets/images/loading.gif";
-import {Typography} from "@material-ui/core";
+import {InputLabel, MenuItem, Select, Typography} from "@material-ui/core";
 
 export default function StreamQuality(props) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [saving, setSaving] = useState(false);
-    const [saveText, setSaveText] = useState("Save");
-    const [currentQualityValue, setCurrentQualityValue] = useState("Fetching...");
-    const [qualityValue, setQualityValue] = useState({resolution: 0, fps: 0});
+    const [qualityValue, setQualityValue] = useState({Resolution: 0, Fps: 0});
 
 
     useEffect(() => {
-        //getCurrentQualityOptions();
-        console.log(props.streamQuality);
+        console.log(JSON.stringify(props.streamQuality));
         if (props.streamQuality.Resolution !== undefined && props.streamQuality.Fps !== undefined) {
-            setCurrentQualityValue(props.streamQuality.Resolution + "p " + props.streamQuality.Fps + " fps");
-        } else {
-            setCurrentQualityValue("Highest Quality.")
+            setQualityValue(props.streamQuality);
         }
     }, [props.streamQuality])
 
-    /*async function getCurrentQualityOptions() {
-        const request = await fetch('setup/quality', {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        
-        if (request.status === 200) {
-            var response = await request.json();
-            setCurrentQualityValue(response.Resolution + "p " + response.Fps + " fps");
-        } else if (request.status === 204) {
-            setQualityValue({resolution: 0, fps: 0})
-            setCurrentQualityValue("Highest Quality.")
-        }
-    }*/
-
-    function handleInputChange(e) {
+    async function handleInputChange(e) {
         var parsedInput = JSON.parse(e.target.value);
-        setQualityValue({resolution: parsedInput.resolution, fps: parsedInput.fps})
-        setSaveText("Save");
-    }
-
-    async function handleSaveClick() {
-        setIsLoading(true);
-        setSaving(true);
-        setSaveText("Saving..")
-
 
         const response = await fetch('setup/globalSettings', {
             method: 'put',
@@ -56,21 +22,14 @@ export default function StreamQuality(props) {
             },
             body: JSON.stringify({
                 "streamQuality": {
-                    "resolution": qualityValue.resolution,
-                    "fps": qualityValue.fps
+                    "resolution": parsedInput.Resolution,
+                    "fps": parsedInput.Fps
                 }
             })
         });
 
         if (response.ok) {
-            setIsLoading(false);
-            setSaving(false);
-            setSaveText("Saved!");
-            if (qualityValue.resolution === 0 && qualityValue.fps === 0) {
-                setCurrentQualityValue("Highest Quality.")
-            } else {
-                setCurrentQualityValue(qualityValue.resolution + "p " + qualityValue.fps + " fps");
-            }
+            setQualityValue({Resolution: parsedInput.Resolution, Fps: parsedInput.Fps})
         }
     }
 
@@ -78,21 +37,21 @@ export default function StreamQuality(props) {
         <div>
             <Typography variant={"h3"} color={"primary"}>Stream Quality</Typography>
             <p>Set the default stream quality for all streams. Streamer level orderrides this settings.</p>
-            <p>Current default quality: {currentQualityValue}</p>
-            <select name="qualityOptions" onChange={handleInputChange}>
-                <option value='{"resolution": 0, "fps": 0}'>Highest Quality</option>
-                <option value='{"resolution": 1080, "fps": 60}'>1080p 60fps</option>
-                <option value='{"resolution": 720, "fps": 60}'>720p 60fps</option>
-                <option value='{"resolution": 720, "fps": 30}'>720p 30fps</option>
-                <option value='{"resolution": 480, "fps": 30}'>480p 30fps</option>
-                <option value='{"resolution": 360, "fps": 30}'>360p 30fps</option>
-                <option value='{"resolution": 160, "fps": 30}'>160p 30fps</option>
-            </select>
+            <InputLabel>Quality Value</InputLabel>
+            <Select
+                value={JSON.stringify(qualityValue)}
+                onChange={handleInputChange}
+                >
+                <MenuItem value='{"Resolution":0,"Fps":0}'>Highest Quality</MenuItem>
+                <MenuItem value='{"Resolution":1080,"Fps":60}'>1080p 60fps</MenuItem>
+                <MenuItem value='{"Resolution":720,"Fps":60}'>720p 60fps</MenuItem>
+                <MenuItem value='{"Resolution":720,"Fps":30}'>720p 30fps</MenuItem>
+                <MenuItem value='{"Resolution":480,"Fps":30}'>480p 30fps</MenuItem>
+                <MenuItem value='{"Resolution":360,"Fps":30}'>360p 30fps</MenuItem>
+                <MenuItem value='{"Resolution":160,"Fps":30}'>160p 30fps</MenuItem>
+            </Select>
             <p>Default is highest quality.</p>
             <p>If the quality is not availble the next best, lower quality will be chosen, or the same resolution but differrent fps will be chosen. More details can be found here.</p>
-            <button onClick={handleSaveClick} disabled={saving}><img
-                className={isLoading ? 'loading' : 'hidden'} alt="loading" src={loading}/>{saveText}
-            </button>
         </div>
     )
 }
