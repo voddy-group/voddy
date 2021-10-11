d1d305be31e9be4fb5c06cd9db465a4490bbd1ea
Patrick <44092756+vigetious@users.noreply.github.com>
2021-10-11 10:45:12 +0100

    fixed deleting stream chat and catching errors

--- Controllers/Streams/DeleteStreamsLogic.cs
+++ Controllers/Streams/DeleteStreamsLogic.cs
@@ -13,48 +13,52 @@ using voddy.Exceptions.Quartz;
 namespace voddy.Controllers.Streams {
     public class DeleteStreamsLogic {
         private static Logger _logger { get; set; } = new NLog.LogFactory().GetCurrentClassLogger();
+
         public DeleteStreamReturn DeleteSingleStreamLogic(long streamId) {
             using (var context = new MainDataContext()) {
-                using (var chatContext = new ChatDataContext()) {
-                    var stream = context.Streams.FirstOrDefault(item => item.streamId == streamId);
-                    if (stream == null) {
-                        stream = context.Streams.FirstOrDefault(item => item.vodId == streamId); // add live stream delete capabilities
-                    }
-                    var chat = chatContext.Chats.Where(item => item.streamId == streamId).ToList();
+                var stream = context.Streams.FirstOrDefault(item => item.streamId == streamId);
+                if (stream == null) {
+                    stream = context.Streams.FirstOrDefault(item => item.vodId == streamId); // add live stream delete capabilities
+                }
 
-                    if (stream != null) {
-                        if (stream.downloadJobId != null) {
-                            var splitJobKey = stream.downloadJobId.Split(".");
-                            try {
-                                JobHelpers.CancelJob(splitJobKey[1], splitJobKey[0], QuartzSchedulers.PrimaryScheduler());
-                            } catch (MissingJobException e) {
-                                _logger.Info(e.Message);
-                            }
+                if (stream != null) {
+                    if (stream.downloadJobId != null) {
+                        var splitJobKey = stream.downloadJobId.Split(".");
+                        try {
+                            JobHelpers.CancelJob(splitJobKey[1], splitJobKey[0], QuartzSchedulers.PrimaryScheduler(), true);
+                        } catch (MissingJobException e) {
+                            _logger.Info(e.Message);
                         }
+                    }
 
-                        if (stream.vodId != 0) {
-                            try {
-                                CleanUpStreamFiles(GlobalConfig.GetGlobalConfig("contentRootPath"), stream.vodId, stream.streamerId);
-                            } catch (DirectoryNotFoundException) {
-                                CleanUpStreamFiles(GlobalConfig.GetGlobalConfig("contentRootPath"), stream.streamId, stream.streamerId);
-                            }
-                        } else {
+                    if (stream.vodId != 0) {
+                        try {
+                            CleanUpStreamFiles(GlobalConfig.GetGlobalConfig("contentRootPath"), stream.vodId, stream.streamerId);
+                        } catch (DirectoryNotFoundException) {
                             CleanUpStreamFiles(GlobalConfig.GetGlobalConfig("contentRootPath"), stream.streamId, stream.streamerId);
                         }
+                    } else {
+                        CleanUpStreamFiles(GlobalConfig.GetGlobalConfig("contentRootPath"), stream.streamId, stream.streamerId);
+                    }
 
-                        context.Remove(stream);
+                    context.Remove(stream);
 
-                        if (stream.chatDownloadJobId != null) {
-                            BackgroundJob.Delete(stream.chatDownloadJobId);
-                            for (var x = 0; x < chat.Count; x++) {
-                                chatContext.Remove(chat[x]);
-                            }
+                    if (stream.chatDownloadJobId != null) {
+                        var splitJobKey = stream.chatDownloadJobId.Split(".");
+                        try {
+                            JobHelpers.CancelJob(splitJobKey[1], splitJobKey[0], QuartzSchedulers.PrimaryScheduler(), true);
+                        } catch (MissingJobException e) {
+                            _logger.Info(e.Message);
                         }
                     }
 
-                    context.SaveChanges();
-                    chatContext.SaveChanges();
+                    using (var chatContext = new ChatDataContext()) {
+                        chatContext.Chats.RemoveRange(chatContext.Chats.Where(item => item.streamId == streamId));
+                        chatContext.SaveChanges();
+                    }
                 }
+
+                context.SaveChanges();
             }
 
             TwitchApiHelpers twitchApiHelpers = new TwitchApiHelpers();
